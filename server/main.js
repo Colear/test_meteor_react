33bad2e877cc7384e2c294b90713669404c6cd7c
Colear <coleargol@gmail.com>
2020-12-13 11:35:24 +0100

    init Mongo

--- server/main.js
+++ server/main.js
@@ -1,31 +1,20 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { TasksCollection } from '/imports/api/TasksCollection';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
+const insertTask = taskText => TasksCollection.insert( { text: taskText });
 
-Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
+Meteor.startup( () => {
 
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
+  // si au démarrage de l'appli la collection est vide on ajoute quelques tâches bidons
+  if (TasksCollection.find().count() === 0) {
+    [
+      'La première',
+      'La deuxième',
+      'La quatrième',
+      'Ah non je m\'a gourré, ça c\'est la quatrième',
+      'La cinquième',
+      'La sixième',
+      'Tous en coeur : la ...'
+    ].forEach( insertTask )
   }
 });
