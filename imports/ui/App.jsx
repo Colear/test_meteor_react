51f2b8b2c23cc682274d74be13fbc155307f8be9
Colear <coleargol@gmail.com>
2020-12-13 11:50:47 +0100

    affichage des tâche de Mongo

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
-
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'},
-];
 
 /*
   L'arrow function correspond à function (task) { return '<Task key={ task._id } task={ task }/>' }.
   Avec map elle est appelée pour chaque ligne du tableau tasks.
   Comme il y a un seul paramètre on peut ne pas mettre de parenthèse : task => {...} au lieu de (task) => {...}
 */
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
+export const App = () => {
 
-    <ul>
-      { tasks.map( task => <Task key={ task._id } task={ task }/>) }
-    </ul>
-  </div>
-);
+  // récupération des données dans Mongo
+  const tasks = useTracker( () => TasksCollection.find( {} ).fetch() )
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+
+      <ul>
+        { tasks.map( task => <Task key={ task._id } task={ task }/>) }
+      </ul>
+    </div>
+  );
+}
